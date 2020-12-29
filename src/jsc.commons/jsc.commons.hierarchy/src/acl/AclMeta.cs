ec5055bf9629ae3cd62d5b741d074fa141f5cb52
jsc86 <schlesinger.jacob@gmail.com>
2020-12-29 10:43:38 +0100

    human readable ToString() for AclMeta

--- src/jsc.commons/jsc.commons.hierarchy/src/acl/AclMeta.cs
+++ src/jsc.commons/jsc.commons.hierarchy/src/acl/AclMeta.cs
@@ -4,6 +4,9 @@
 // File authors (in chronological order):
 //  - Jacob Schlesinger <schlesinger.jacob@gmail.com>
 
+using System;
+using System.Text;
+
 using jsc.commons.behaving.interfaces;
 
 namespace jsc.commons.hierarchy.acl {
@@ -11,6 +14,14 @@ namespace jsc.commons.hierarchy.acl {
    public class AclMeta : IBehavior {
 
       public string Acl { get; set; }
+
+      public override string ToString( ) {
+         StringBuilder sb = new StringBuilder( );
+         sb.Append( "meta: ACL:" );
+         sb.Append( Environment.NewLine );
+         sb.Append( Acl );
+         return sb.ToString( );
+      }
 
    }
 
