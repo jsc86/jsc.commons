3360b710bf27199e809a3efa818d63db31b17063
jsc86 <schlesinger.jacob@gmail.com>
2021-01-04 16:11:31 +0100

    added missing file header

--- src/jsc.commons/jsc.commons.hierarchy/src/Events.cs
+++ src/jsc.commons/jsc.commons.hierarchy/src/Events.cs
@@ -1,3 +1,9 @@
+// Licensed under the MIT license.
+// See LICENSE file in the project root directory for full information.
+// Copyright (c) 2020 Jacob Schlesinger
+// File authors (in chronological order):
+//  - Jacob Schlesinger <schlesinger.jacob@gmail.com>
+
 using System.Threading.Tasks;
 
 using jsc.commons.hierarchy.interfaces;
